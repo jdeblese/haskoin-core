6163f12b533a81e48790a6a1ffc90c07e0f13e16
Jan-willem De Bleser <jw@thescrapyard.org>
2022-01-22 14:59:59 -0800

    Fix missing import

--- test/Haskoin/NetworkSpec.hs
+++ test/Haskoin/NetworkSpec.hs
@@ -5,6 +5,7 @@ module Haskoin.NetworkSpec (spec) where
 import Data.Bytes.Get
 import Data.Bytes.Put
 import Data.Bytes.Serial
+import Data.List ((\\))
 import qualified Data.Map as Map
 import Data.Maybe (fromJust)
 import Data.Text (Text)
